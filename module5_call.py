fad30fa91a0acbb4e119c75e196e5249ff265521
WSUFan <fan.ye@wsu.edu>
2023-11-01 20:32:13 +0000

    Assignment #5.2 Problem 2

---
+++ module5_call.py
@@ -0,0 +1,7 @@
+from module5_mod import process_interactive_with_stdin
+
+def main():
+    process_interactive_with_stdin()
+
+if __name__ == "__main__":
+    main()
---
+++ module5_mod.py
@@ -0,0 +1,37 @@
+class DataProcessor:
+    def __init__(self):
+        self.numbers = []
+        self.N = 0
+    
+    def init_from_stdin(self):
+        self._init_len_from_stdin()
+        self._init_data_from_stdin()
+
+    def _init_len_from_stdin(self):
+        N = int(input("Enter a positive integer N: "))
+        while N <= 0:
+            N = int(input("Please enter a positive integer N: "))
+        self.N = N
+    
+    def _init_data_from_stdin(self):
+        for _ in range(self.N):
+            num = int(input("Please enter a number: "))
+            self.numbers.append(num)
+    
+    def search_data(self, X):
+        indices = [i + 1 for i, num in enumerate(self.numbers) if num == X]
+        return indices
+
+def process_interactive_with_stdin():
+    data_processor = DataProcessor()
+    
+    data_processor.init_from_stdin()
+    
+    X = int(input("Please enter an integer X for search: "))
+    indices = data_processor.search_data(X)
+    
+    if len(indices) > 0:
+        print(indices)
+    else:
+        print("-1")
+
